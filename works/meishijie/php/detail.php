5aae631a8874910e29bc06f42b73b04f974e18b1
mengxiaoxi <819908233@qq.com>
2016-11-12 20:59:51 +0800

    key and style

--- works/meishijie/php/detail.php
+++ works/meishijie/php/detail.php
@@ -1,6 +1,6 @@
 <?php
 	$data =$_POST['data'];
-	$key="0cf92c915763bc2297f542100e49f3c8";
+	$key="eb797409df0333f982c5ec5e0a9384b8";
 	header("Access-Control-Allow-Origin:http://apis.juhe.cn/cook/queryid?key=".$key."&".$data);
 	header('Access-Control-Allow-Methods:POST');
 	$url ="http://apis.juhe.cn/cook/queryid?key=".$key."&".$data;
--- works/meishijie/php/menutype.php
+++ works/meishijie/php/menutype.php
@@ -1,5 +1,5 @@
 <?php
-	$key="0cf92c915763bc2297f542100e49f3c8";
+	$key="eb797409df0333f982c5ec5e0a9384b8";
 	header("Access-Control-Allow-Origin:http://apis.juhe.cn/cook/category?key=".$key);
 	header('Access-Control-Allow-Methods:POST');
 	$url ="http://apis.juhe.cn/cook/category?key=".$key;
